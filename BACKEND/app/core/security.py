084b52a04e11c72d57caf454b61129141373464f
GordoMatti <matinicoher@gmail.com>
2026-06-02 11:03:35 -0300

    agregamos logica de direccion

--- BACKEND/app/core/security.py
+++ BACKEND/app/core/security.py
@@ -5,7 +5,7 @@ from jose import jwt
 from passlib.context import CryptContext
 from app.core.config import settings
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto", rounds=12)
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto",bcrypt__rounds=12)
 
 
 def hash_password(plain: str) -> str:
--- BACKEND/app/modules/dominio_1/Usuarios/schemas.py
+++ BACKEND/app/modules/dominio_1/Usuarios/schemas.py
@@ -45,35 +45,37 @@ class LoginInput(SQLModel):
     password: str
 
 class DireccionCreate(SQLModel):
-    alias:         str = Field(min_length=1, max_length=50)
-    linea1:        str
-    linea2:        Optional[str] = None
-    ciudad:        str = Field(min_length=1, max_length=100)
-    provincia:     str
-    codigo_postal: str = Field(max_length=10)
-    latitud:       Optional[Decimal] = None
-    longitud:      Optional[Decimal] = None
- 
- 
+    alias: Optional[str] = Field(default=None, max_length=50)
+    linea2: Optional[str] = None
+    provincia: Optional[str] = Field(default=None, max_length=100)
+    codigo_postal: Optional[str] = Field(default=None, max_length=10)
+    latitud: Optional[Decimal] = None
+    longitud: Optional[Decimal] = None
+    
+    linea1: str
+    ciudad: str = Field(min_length=1, max_length=100)
+    
+    es_principal: bool = False
+    
 class DireccionUpdate(SQLModel):
-    alias:         Optional[str] = None
-    linea1:        Optional[str] = None
-    linea2:        Optional[str] = None
-    ciudad:        Optional[str] = None
-    provincia:     Optional[str] = None
-    codigo_postal: Optional[str] = None
- 
+    alias: Optional[str] = Field(default=None, max_length=50)
+    linea1: Optional[str] = None
+    linea2: Optional[str] = None
+    ciudad: Optional[str] = Field(default=None, max_length=100)
+    provincia: Optional[str] = Field(default=None, max_length=100)
+    codigo_postal: Optional[str] = Field(default=None, max_length=10)
+    es_principal: Optional[bool] = None
  
 class DireccionRead(SQLModel):
-    id:            int
-    usuario_id:    int
-    alias:         str
-    linea1:        str
-    linea2:        Optional[str]
-    ciudad:        str
-    provincia:     str
-    codigo_postal: str
-    es_principal:  bool
-    deleted_at:    Optional[datetime]
+    id: int
+    usuario_id: int
+    alias: Optional[str] = None
+    linea1: str
+    linea2: Optional[str] = None
+    ciudad: str
+    provincia: Optional[str] = None
+    codigo_postal: Optional[str] = None
+    es_principal: bool
+    deleted_at: Optional[datetime] = None
 
  
